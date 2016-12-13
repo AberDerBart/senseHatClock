1a78d24ea00b02cb86c8baf2ed0868a0e0b791a6
AberDerBart <nonatz@web.de>
2016-12-14 00:42:22 +0100

    implemented experimental menu

--- main.py
+++ main.py
@@ -4,6 +4,31 @@ sense=SenseHat()
 
 run=True
 
+sleepImage=sense.load_image("sleep.png")
+alarmImage=sense.load_image("alarm.png")
+ctlImage=sense.load_image("ctl.png")
+volImage=sense.load_image("vol.png")
+
+selection=None
+
+directionMap={
+	'right':sleepImage,
+	'left':alarmImage,
+	'up':ctlImage,
+	'down':volImage,
+	'middle':None
+}
+
 while(run):
 	event=sense.stick.wait_for_event()
+	
+	# update the selected menu
+	if(event.action=='pressed'):
+		selection=directionMap[event.direction]
 
+	if(selection):
+		sense.set_pixels(selection)
+
+
+
+	
