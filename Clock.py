1bed0fa7424e5a8268ddac9c2c5ffe497b773f68
AberDerBart <nonatz@web.de>
2016-12-14 14:31:40 +0100

    braille: implemented clock

---
+++ Clock.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import time
+import braille
+
+class Clock:
+	def __init__(self,sense):
+		self.sense=sense
+	def update(self):
+		localTime=time.localtime()
+		hourString=str(localTime.tm_hour).rjust(2,'0')
+		minString=str(localTime.tm_min).rjust(2,'0')
+		self.sense.clear()
+		braille.printB(self.sense,0,0,hourString)
+		braille.printB(self.sense,3,4,minString)
+	def up(self):
+		pass
+	def down(self):
+		pass
+	def left(self):
+		pass
+	def right(self):
+		pass
+	def center(self):
+		pass
