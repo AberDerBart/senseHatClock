bbaf713a25da4b9d73cf53ff304c66abb65e2a7e
AberDerBart <nonatz@web.de>
2016-12-28 18:01:13 +0100

    adjusted clock to new Ui

--- Clock.py
+++ Clock.py
@@ -2,25 +2,29 @@
 import time
 import braille
 
+import Ui
+
 class Clock:
-	def __init__(self,sense):
-		self.sense=sense
+	def __init__(self):
+		pass
 	def update(self):
 		localTime=time.localtime()
 		hourString=str(localTime.tm_hour).rjust(2,'0')
 		minString=str(localTime.tm_min).rjust(2,'0')
 		self.sense.clear()
-		braille.printB(self.sense,0,0,hourString)
-		braille.printB(self.sense,3,4,minString)
+		braille.printB(Ui.sense,0,0,hourString)
+		braille.printB(Ui.sense,3,4,minString)
 	def open(self):
 		pass
 	def close(self):
 		pass
-	def down(self):
+	def down(self,event):
 		pass
-	def left(self):
+	def left(self,event):
 		pass
-	def right(self):
+	def right(self,event):
 		pass
-	def middle(self):
+	def middle(self,event):
 		pass
+
+Ui.registerApp("clock",Clock())
