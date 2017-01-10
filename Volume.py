7a518a3e865d98f91725dec3b76611160350c4f1
AberDerBart <nonatz@web.de>
2017-01-10 20:48:44 +0100

    implemented volume

--- Volume.py
+++ Volume.py
@@ -3,4 +3,33 @@ import Ui
 from Player import Player
 import Menu
 
-Ui.registerApp("volume",Menu.Menu("img/playerAdvanced/volume.png",upAction="player"))
+class PlayerVolume:
+	def __init__(self):
+		self.image=Ui.sense.load_image("img/playerAdvanced/volume.png")
+	def open(self):
+		Ui.drawLoading()
+		Player.client.connect(Player.host,Player.port)
+		self.update()
+		self.volume=None
+	def close(self):
+		Player.client.disconnect()
+	def update(self):
+		Ui.drawImage(self.image)
+		if("volume" in Player.client.status()):
+			self.volume=int(Player.client.status()["volume"])
+			
+	def down(self,event):
+		pass
+	def left(self,event):
+		if(event.action=='pressed'):
+			Player.client.setvol(max(self.volume-1,0))
+	def right(self,event):
+		if(event.action=='pressed'):
+			Player.client.setvol(min(self.volume+1,100))
+	def middle(self,event):
+		pass
+	def up(self,event):
+		if(event.action=="pressed"):
+			Ui.setApp("player")
+
+Ui.registerApp("volume",PlayerVolume())
